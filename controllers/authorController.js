5c46a885f5c1b3fce20ba03aaa77a1ff8ad59147
Jialin000 <61930223+Jialin000@users.noreply.github.com>
2020-04-25 20:56:44 +1000

    Update authorController.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,13 +3,32 @@ const mongoose = require("mongoose");
 // import author model
 const Author = mongoose.model("author");
 
+var url = "mongodb://localhost: 27017/info30005";
+
     
 // function to handle a request to get all authors
+// const getAllAuthors = async (req, res) => {
+//   try {
+//     const all_authors = await Author.find();
+//     return res.send(all_authors);
+//   } catch (err) {
+//     res.status(400);
+//     return res.send("Database query failed");
+//   }
+// };
+
 const getAllAuthors = async (req, res) => {
-    
+  var resultArray = [];
   try {
-    const all_authors = await Author.find();
-    return res.send(all_authors);
+    mongoose.connect(url, function(err, db){
+      var cursor = db.collection('author').find();
+      cursor.forEach(function(docm, err){
+        resultArray.push(doc);
+      }),function(){
+        db.close()
+      };
+    });
+    return res.send(resultArray);
   } catch (err) {
     res.status(400);
     return res.send("Database query failed");
@@ -26,6 +45,15 @@ const updateAuthor = async (req, res) => {
 
 // function to add author
 const addAuthor = async (req, res) => {
+  var item = {
+    id: req.body.id,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  }
+  mongoose.connect(url, function(err, db){
+    db.collection('author').insertOne(item);
+    db.close();
+  });
  res.send("Working on this feature");
 };
 
